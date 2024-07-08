5a8d3910f031821668b18efde17ac6cfb97828b4
David Vicente <davidvicento@gmail.com>
2024-07-08 21:46:57 +0200

    fix: fix remplace text

--- src/utils.js
+++ src/utils.js
@@ -34,38 +34,7 @@ function getHtml(url) {
 }
 
 /**
- * Get the actual selected text
- * @returns {string}
- */
-function getSelectedExpression() {
-	let doc = DocumentApp.getActiveDocument()
-	let selection = doc.getSelection()
-	if (selection) {
-		return "Errer"
-	}
-
-	let elements = selection.getRangeElements()
-	if (elements.length != 1) {
-		return "Errer"
-	}
-
-	// Get all the text from element
-	let selectedText = elements[0].getElement().asText().getText()
-
-	// Get only the selected text
-	if (elements[0].isPartial()) {
-		let startOffset = elements[0].getStartOffset()
-		let endOffset = elements[0].getEndOffsetInclusive()
-		selectedText = selectedText.substring(startOffset, endOffset + 1)
-	}
-
-	return selectedText.trim()
-}
-
-/**
- * Gets the text the user has selected. If there is no selection,
- * this function displays an error message
- *
+ * Gets the text the user has selected
  * @return {string} The selected text
  */
 function getSelectedText() {
@@ -82,12 +51,10 @@ function getSelectedText() {
 				text.push(element.getText().substring(startIndex, endIndex + 1))
 			} else {
 				const element = elements[i].getElement()
-				// Only translate elements that can be edited as text; skip images and
-				// other non-text elements.
+				// Only translate elements that can be edited as text
 				if (element.editAsText) {
 					const elementText = element.asText().getText()
-					// This check is necessary to exclude images, which return a blank
-					// text element.
+					// This check is necessary to exclude images, which return a blank text element
 					if (elementText) {
 						text.push(elementText)
 					}
@@ -100,45 +67,85 @@ function getSelectedText() {
 }
 
 /**
- * Replace document selection by @text
- * @param {string} text
- * @returns
+ * Replaces the text of the current selection with the provided text, or
+ * inserts text at the current cursor location. (There will always be either
+ * a selection or a cursor.) If multiple elements are selected, only inserts the
+ * translated text in the first element that can contain text and removes the
+ * other elements.
+ *
+ * @param {string} newText The text with which to replace the current selection.
  */
-function replaceSelection(text) {
-	let doc = DocumentApp.getActiveDocument()
-	let selection = doc.getSelection()
+function insertText(newText) {
+	const selection = DocumentApp.getActiveDocument().getSelection()
 	if (selection) {
-		return "Errer"
-	}
-
-	let elements = selection.getRangeElements()
-	if (elements.length != 1) {
-		return "Errer"
-	}
-
-	let element = elements[0].getElement()
-	let startOffset = elements[0].getStartOffset() // -1 if whole element
-	let endOffset = elements[0].getEndOffsetInclusive() // -1 if whole element
-
-	if (startOffset != -1 && endOffset != -1) {
-		let selectedText = element.asText().getText()
-		// Add space at end
-		if (selectedText[endOffset] == " ") {
-			text += " "
+		let replaced = false
+		const elements = selection.getRangeElements()
+		if (elements.length === 1 && elements[0].getElement().getType() === DocumentApp.ElementType.INLINE_IMAGE) {
+			throw new Error("Can't insert text into an image.")
 		}
-		// Upper case
-		if (selectedText[startOffset] == selectedText[startOffset].toUpperCase()) {
-			text = firstUpperCase(text)
+		for (let i = 0; i < elements.length; ++i) {
+			if (elements[i].isPartial()) {
+				const element = elements[i].getElement().asText()
+				const startIndex = elements[i].getStartOffset()
+				const endIndex = elements[i].getEndOffsetInclusive()
+				element.deleteText(startIndex, endIndex)
+				if (!replaced) {
+					element.insertText(startIndex, newText)
+					replaced = true
+				} else {
+					// This block handles a selection that ends with a partial element. We
+					// want to copy this partial text to the previous element so we don't
+					// have a line-break before the last partial.
+					const parent = element.getParent()
+					const remainingText = element.getText().substring(endIndex + 1)
+					parent.getPreviousSibling().asText().appendText(remainingText)
+					// We cannot remove the last paragraph of a doc. If this is the case,
+					// just remove the text within the last paragraph instead.
+					if (parent.getNextSibling()) {
+						parent.removeFromParent()
+					} else {
+						element.removeFromParent()
+					}
+				}
+			} else {
+				const element = elements[i].getElement()
+				if (!replaced && element.editAsText) {
+					// Only translate elements that can be edited as text, removing other
+					// elements.
+					element.clear()
+					element.asText().setText(newText)
+					replaced = true
+				} else {
+					// We cannot remove the last paragraph of a doc. If this is the case,
+					// just clear the element.
+					if (element.getNextSibling()) {
+						element.removeFromParent()
+					} else {
+						element.clear()
+					}
+				}
+			}
 		}
-		// Replace text
-		element.editAsText().deleteText(startOffset, endOffset)
-		element.editAsText().insertText(startOffset, text)
 	} else {
-		text = firstUpperCase(text)
-		element.setText(text)
-	}
+		const cursor = DocumentApp.getActiveDocument().getCursor()
+		const surroundingText = cursor.getSurroundingText().getText()
+		const surroundingTextOffset = cursor.getSurroundingTextOffset()
 
-	return true
+		// If the cursor follows or preceds a non-space character, insert a space
+		// between the character and the translation. Otherwise, just insert the
+		// translation.
+		if (surroundingTextOffset > 0) {
+			if (surroundingText.charAt(surroundingTextOffset - 1) !== " ") {
+				newText = " " + newText
+			}
+		}
+		if (surroundingTextOffset < surroundingText.length) {
+			if (surroundingText.charAt(surroundingTextOffset) !== " ") {
+				newText += " "
+			}
+		}
+		cursor.insertText(newText)
+	}
 }
 
 /* ----- SIMPLE METHODS ----- */
